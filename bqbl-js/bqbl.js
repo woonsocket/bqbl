c5a05ee6553223a54e749f24e3acc4d0c2d336cf
aerion <aerion@gmail.com>
2012-10-07 22:44:50 +0000

    Timeout value should be in msecs, not seconds. Duuuuuhhhhh

--- bqbl-js/bqbl.js
+++ bqbl-js/bqbl.js
@@ -39,7 +39,7 @@ bqbl.loadAndUpdate = function(url) {
       {
         'Cache-Control': 'max-age: 60'
       },
-      60  // opt_timeoutInterval
+      60 * 1000  // opt_timeoutInterval
       );
 };
 
