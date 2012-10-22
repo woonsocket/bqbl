a23cabde4396c37b316aa9c8ceadd589707a24fe
aerion <aerion@gmail.com>
2012-10-22 20:52:21 +0000

    A script for grabbing game IDs off of ESPN so we can generate box score links automatically.

---
+++ boxscore-link-scrape.py
@@ -0,0 +1,25 @@
+# Grabs the game IDs for all the NFL games in a given week and outputs
+# the box score URLs for the games.
+# Usage: %prog <weeknum>
+
+import re
+import sys
+import urllib
+
+WEEKLY_SCOREBOARD_URL_PATTERN = (
+    'http://scores.espn.go.com/nfl/scoreboard?seasonYear=2012&seasonType=2'
+    '&weekNumber=%s')
+
+BOX_SCORE_URL_PATTERN = (
+    'http://scores.espn.go.com/nfl/boxscore?gameId=%s')
+
+week_num = int(sys.argv[1])
+
+scoreboard_data = urllib.urlopen(
+    WEEKLY_SCOREBOARD_URL_PATTERN % week_num).read()
+
+GAME_ID_RE = re.compile('class="sort".*?>([\d]+?)<')
+matches = GAME_ID_RE.findall(scoreboard_data)
+
+for match in matches:
+    print BOX_SCORE_URL_PATTERN % match
