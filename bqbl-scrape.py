885a973de7cd70740b9d6c61411b0a2af8c83948
aerion <aerion@gmail.com>
2016-09-12 05:36:18 +0000

    Fix another instance of STL -> LA.

--- bqbl-scrape.py
+++ bqbl-scrape.py
@@ -28,8 +28,8 @@ if options.output_format not in OUTPUT_FORMATS:
 
 
 ALL_TEAMS = ("ARI","ATL","BAL","BUF","CAR","CHI","CIN","CLE","DAL","DEN","DET",
-             "GB","HOU","IND","JAX","KC","MIA","MIN","NE","NO","NYG","NYJ",
-             "OAK", "PHI","PIT","SD","SEA","SF","STL","TB","TEN","WSH")
+             "GB","HOU","IND","JAX","KC","LA","MIA","MIN","NE","NO","NYG","NYJ",
+             "OAK", "PHI","PIT","SD","SEA","SF","TB","TEN","WSH")
 
 
 # Gross global variables
