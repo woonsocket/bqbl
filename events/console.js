2c2e14a3ad4c2e7ab4829ea53c6f6ec87ae66afc
Harvey Jones <harveyj@gmail.com>
2017-07-30 09:10:42 -0400

    add benchings, cleanups
    Add a benchings console
    fix embarrassing typos from last night
    generalize “i” to “id”

--- events/console.js
+++ events/console.js
@@ -16,8 +16,8 @@ document.addEventListener('DOMContentLoaded', function() {
 function onEventLoad(snapshot) {
 	var fumbles = snapshot.val().fumbles;
 	for (var i = 0; i < fumbles.length; i++) {
-		var safety = fumbles[i];
-		var row = makeRow(safety, i, "events/2016/1/fumbles");
+		var fumble = fumbles[i];
+		var row = makeRow(fumble, i, "events/2016/1/fumbles");
 		document.querySelector("#fumble_list").append(row);
 	}
 
@@ -27,18 +27,29 @@ function onEventLoad(snapshot) {
 		var row = makeRow(safety, i, "events/2016/1/safeties");
 		document.querySelector("#safety_list").append(row);
 	}
+
+	var allTeams = ["ARI","ATL","BAL","BUF","CAR","CHI","CIN","CLE","DAL","DEN","DET",
+									"GB","HOU","IND","JAX","KC","LA","MIA","MIN","NE","NO","NYG","NYJ",
+									"OAK", "PHI","PIT","SD","SEA","SF","TB","TEN","WSH"];
+	var benchings = snapshot.val().benchings;
+	console.log(benchings);
+	allTeams.forEach(function(team) {
+			var valid = benchings[team] && benchings[team].valid;
+			var row = makeRow({"desc": team, "valid": valid}, team, "events/2016/1/benchings");
+			document.querySelector("#benchings_list").append(row);
+		});
 }
 
-function makeRow(item, i, path) {
+function makeRow(item, id, path) {
 	var tpl = document.querySelector("#radio_tpl").cloneNode(true);
 	var descElement = tpl.querySelector("span.desc");
 	var inputElement = tpl.querySelector("input");
 	
-	tpl.setAttribute("id", i);
+	tpl.setAttribute("id", id);
 	descElement.textContent = item.desc;
 	inputElement.checked = item.valid;
 	tpl.querySelector("input").onclick =
-		clickHandler.bind(null, i, path, inputElement);
+		clickHandler.bind(null, id, path, inputElement);
 	return tpl;
 };
 
