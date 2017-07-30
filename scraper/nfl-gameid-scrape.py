9128718677225f4ea1835501208775af76b73ee3
Harvey Jones <harveyj@gmail.com>
2017-07-30 10:20:57 -0400

    make game id scrape output to a sane loc by default

--- scraper/nfl-gameid-scrape.py
+++ scraper/nfl-gameid-scrape.py
@@ -4,7 +4,7 @@ import re
 
 parser = optparse.OptionParser(
   usage=("Usage: %prog [options] "))
-parser.add_option("-o", "--output_file", dest="output_file", default="tab",
+parser.add_option("-o", "--output_file", dest="output_file", default=None,
                   help="Output file")
 parser.add_option("-w", "--week", dest="week", default="1",
                   help="Week")
@@ -21,7 +21,10 @@ ids = {}
 for id in re.findall(pat, raw):
     ids[id] = 1
 
-f = open(options.output_file, "w")
+output_file = options.output_file
+if not output_file:
+    output_file = "%s-%s-gameids.txt" % (options.year, options.week)
+f = open(output_file, "w")
 for id in ids.keys():
     f.write(id + "\n")
 
