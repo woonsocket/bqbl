5059b7d64dcdb90a86cbfcbf08b7825b41fffca2
Harvey Jones <harveyj@gmail.com>
2017-07-30 10:23:01 -0400

    Add arbitrary year, week to console. Material design-ize the header
    Year and week are read from year= and week= in the hash.

--- events/console.js
+++ events/console.js
@@ -1,3 +1,5 @@
+args = splitHash();
+
 document.addEventListener('DOMContentLoaded', function() {
 		// Initialize Firebase
 		var config = {
@@ -10,22 +12,26 @@ document.addEventListener('DOMContentLoaded', function() {
 		};
 		firebase.initializeApp(config);
 
-		firebase.database().ref("events/2016/1").once('value').then(onEventLoad);
+		firebase.database().ref(getEventsPath()).once('value').then(onEventLoad);
 	});
+
+function getEventsPath() {
+	return "events/" + args.year + "/" + args.week;
+}
 
 function onEventLoad(snapshot) {
 	var fumbles = snapshot.val().fumbles;
 	for (var i = 0; i < fumbles.length; i++) {
 		var fumble = fumbles[i];
-		var row = makeRow(fumble, i, "events/2016/1/fumbles");
-		document.querySelector("#fumble_list").append(row);
+		var row = makeRow(fumble, i, getEventsPath() + "/fumbles");
+		document.querySelector("#fumbles_list").append(row);
 	}
 
 	var safeties = snapshot.val().safeties;
 	for (var i = 0; i < safeties.length; i++) {
 		var safety = safeties[i];
-		var row = makeRow(safety, i, "events/2016/1/safeties");
-		document.querySelector("#safety_list").append(row);
+		var row = makeRow(safety, i, getEventsPath() + "/safeties");
+		document.querySelector("#safeties_list").append(row);
 	}
 
 	var allTeams = ["ARI","ATL","BAL","BUF","CAR","CHI","CIN","CLE","DAL","DEN","DET",
@@ -35,13 +41,13 @@ function onEventLoad(snapshot) {
 	console.log(benchings);
 	allTeams.forEach(function(team) {
 			var valid = benchings[team] && benchings[team].valid;
-			var row = makeRow({"desc": team, "valid": valid}, team, "events/2016/1/benchings");
+			var row = makeRow({"desc": team, "valid": valid}, team, getEventsPath() + "/benchings");
 			document.querySelector("#benchings_list").append(row);
 		});
 }
 
 function makeRow(item, id, path) {
-	var tpl = document.querySelector("#radio_tpl").cloneNode(true);
+	var tpl = document.querySelector("#item_tpl").cloneNode(true);
 	var descElement = tpl.querySelector("span.desc");
 	var inputElement = tpl.querySelector("input");
 	
@@ -58,3 +64,15 @@ function clickHandler(id, path, inputElement) {
   updates[path + "/" + id + "/valid"] = inputElement.checked;
   return firebase.database().ref().update(updates);
 };
+
+// Take window.location.hash and turn it into a key:val map.
+function splitHash() {
+	// Strip leading # and split by &
+	var items = window.location.hash.slice(1).split("&");
+	var ret = {};
+	items.forEach(function (item) {
+			var vals = item.split("=");
+			ret[vals[0]] = vals[1];
+		});
+	return ret;
+}
