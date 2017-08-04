c548d063585be9dfc3e67fe1f39e065d3840ae25
Aerion <aerion@gmail.com>
2017-08-04 11:05:42 -0700

    Move play processing into an object.
    Print to stdout if not uploading to Firebase.

--- events/pbp-scrape.py
+++ events/pbp-scrape.py
@@ -30,55 +30,67 @@ if len(args) < 1:
   parser.print_usage()
   sys.exit(1)
 
-def process(raw, fumbles, safeties, interceptions):
-  data = json.loads(str(raw, 'utf-8'))
-  drives = data[id]['drives']
-  # 5 is for overtime
-  quarters = {1: {}, 2: {}, 3:{}, 4:{}, 5:{}}
 
-  for drive in drives:
-    # skip junk in there about current drive
-    if drive == 'crntdrv': continue
-    plays = copy.deepcopy(drives[drive]['plays'])
-    for play_key in plays:
-      play = plays[play_key]
-      quarter = play['qtr']
-      time = play['time']
-      quarters[quarter][time] = play
+class Plays(object):
 
-  for n in range(1, 6):
-    for t in sorted(quarters[n].keys())[::-1]:
-      play = quarters[n][t]
-      desc = play['desc']
-      if "SAFETY" in desc:
-        safeties.append({
-            'desc': desc, 'team': play['posteam'], 'quarter': n, 'time': t})
-      elif "FUMBLE" in desc and "TOUCHDOWN" in desc:
-        fumbles.append({
-            'desc': desc, 'team': play['posteam'], 'quarter': n, 'time': t})
-      if "INTERCEPT" in desc:
-        interceptions.append({
-            'desc': desc, 'team': play['posteam'], 'quarter': n, 'time': t})
+  def __init__(self):
+    self.fumbles = []
+    self.safeties = []
+    self.interceptions = []
+
+  def process(self, raw):
+    data = json.loads(str(raw, 'utf-8'))
+    drives = data[id]['drives']
+    # 5 is for overtime
+    quarters = {1: {}, 2: {}, 3:{}, 4:{}, 5:{}}
+
+    for drive in drives:
+      # skip junk in there about current drive
+      if drive == 'crntdrv': continue
+      plays = copy.deepcopy(drives[drive]['plays'])
+      for play_key in plays:
+        play = plays[play_key]
+        quarter = play['qtr']
+        time = play['time']
+        quarters[quarter][time] = play
+
+    for n in range(1, 6):
+      for t in sorted(quarters[n].keys())[::-1]:
+        play = quarters[n][t]
+        desc = play['desc']
+        if "SAFETY" in desc:
+          self.safeties.append({
+              'desc': desc, 'team': play['posteam'], 'quarter': n, 'time': t})
+        elif "FUMBLE" in desc and "TOUCHDOWN" in desc:
+          self.fumbles.append({
+              'desc': desc, 'team': play['posteam'], 'quarter': n, 'time': t})
+        if "INTERCEPT" in desc:
+          self.interceptions.append({
+              'desc': desc, 'team': play['posteam'], 'quarter': n, 'time': t})
 
 
 gameIds = open(args[0]).readlines()
-fumbles = []
-safeties = []
-interceptions = []
 
+plays = Plays()
 for id in gameIds:
   id = id.strip()
   url = "http://www.nfl.com/liveupdate/game-center/%s/%s_gtd.json" % (id, id)
   raw = urllib.request.urlopen("http://www.nfl.com/liveupdate/game-center/%s/%s_gtd.json" % (id, id)).read()
-  process(raw, fumbles, safeties, interceptions)
+  plays.process(raw)
 
 if options.firebase:
   fumble_ref = db.reference('/events/%s/%s/fumbles' % (options.year, options.week))
-  fumble_ref.set(fumbles)
+  fumble_ref.set(plays.fumbles)
 
   safety_ref = db.reference('/events/%s/%s/safeties' % (options.year, options.week))
-  safety_ref.set(safeties)
+  safety_ref.set(plays.safeties)
 
   interception_ref = db.reference('/events/%s/%s/interception' % (options.year, options.week))
-  interception_ref.set(interceptions)
-
+  interception_ref.set(plays.interceptions)
+else:
+  for f in plays.fumbles:
+    print(f)
+  for s in plays.safeties:
+    print(s)
+  for i in plays.interceptions:
+    print(i)
