b58197a2f49497843b8c9ead3125ce48aa3a7f90
Aerion <aerion@gmail.com>
2017-08-16 23:12:29 -0700

    Upgrade Closure compiler and stick it in third_party.
    The version I had been using is probably a few years old. For example, it didn't know about Object.entries.

--- compile.sh
+++ compile.sh
@@ -1,31 +1,17 @@
 #!/bin/sh
 
-# Get the Closure Compiler from http://code.google.com/p/closure-compiler/, and
-# save it as compiler.jar.
 # Get the Closure Library from https://developers.google.com/closure/library/
-# and place it in closure-library, or run:
-#   svn co http://closure-library.googlecode.com/svn/trunk/ closure-library
+# and place it in third_party/closure-library.
 
-# Ubuntu 15.04 started setting JAVA_TOOL_OPTIONS, which makes Java echo a line
-# to stdout when starting. The compiler determines Java version by parsing the
-# output of `java -version`, and the extra line breaks it.
-unset JAVA_TOOL_OPTIONS
-
-tmp=$(mktemp -d)
-closure-library/closure/bin/build/closurebuilder.py \
-    --root=closure-library/ \
-    --root=web/ \
-    --namespace=bqbl \
-    --output_mode=compiled \
-    --compiler_jar=compiler.jar \
-    -f "--js=closure-library/closure/goog/deps.js" \
-    -f "--externs=firebase-externs.js" \
-    -f "--compilation_level=ADVANCED_OPTIMIZATIONS" \
-    -f "--warning_level=VERBOSE" \
-    -f "--jscomp_error=checkTypes" \
-    -f "--jscomp_error=checkVars" \
-    > "${tmp}/bqbl-compiled.js" &&
-    mv "${tmp}/bqbl-compiled.js" web/ &&
+java -jar third_party/closure-compiler/closure-compiler-v20170806.jar \
+    --js="third_party/closure-library/**.js" \
+    --js="web/bqbl.js" \
+    --dependency_mode="STRICT" \
+    --entry_point="bqbl" \
+    --externs="firebase-externs.js" \
+    --compilation_level="ADVANCED_OPTIMIZATIONS" \
+    --warning_level="VERBOSE" \
+    --jscomp_error="checkTypes" \
+    --jscomp_error="checkVars" \
+    --js_output_file="web/bqbl-compiled.js" &&
     wc -c web/bqbl-compiled.js
-
-rm -r "${tmp}"
