fd08d43a50af4e018ef279623c6b331bc4f0d618
Harvey Jones <harveyj@gmail.com>
2017-08-19 19:26:20 -0400

    Cleanup
    git mv fe3 web3
    git rm

--- web-firebase/public/lineup-row.tpl.js
+++
@@ -1,9 +0,0 @@
-<tr>
-	<td class="mdl-data-table__cell--non-numeric">{{week}}</td>
-	{{#each team}}
-    <td class="mdl-data-table__cell--non-numeric {{selected}} cell-listener"
-	     data-index={{@index}}
-	 data-team={{@key}}
-    	 data-week={{@root.week}}>{{@key}}</td>
-  {{/each}}
-</tr>
---
+++ web3/src/environments/environment.prod.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyCVbZ7U5Y4ZO-tsQpsZgIf7ROPJdpAXLuE',
+    authDomain: 'bqbl-591f3.firebaseapp.com',
+    databaseURL: 'https://bqbl-591f3.firebaseio.com',
+    projectId: 'bqbl-591f3',
+    storageBucket: 'bqbl-591f3.appspot.com',
+    messagingSenderId: '983576610595'
+  }
+};
