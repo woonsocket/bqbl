55733538a3ac4e7f49ab8600b78bdcda9977b2fc
Harvey Jones <harveyj@gmail.com>
2017-08-19 16:53:08 -0400

    Add week support to scores

--- fe3/src/app/nflscores.component.ts
+++ fe3/src/app/nflscores.component.ts
@@ -3,19 +3,25 @@ import { Component } from '@angular/core';
 import { AngularFireDatabase, FirebaseListObservable, FirebaseObjectObservable } from 'angularfire2/database';
 import { Observable } from 'rxjs/Observable';
 import * as firebase from 'firebase/app';
+import { ActivatedRoute, Params } from '@angular/router';
 
 @Component({
- templateUrl: './nflscores.component.html',
+  templateUrl: './nflscores.component.html',
 })
 export class NFLScoresComponent {
- scores: FirebaseListObservable<any>;
+  scores: FirebaseListObservable<any>;
+  selectedWeek = 'P1';
+  year = '2017';
+  constructor(private db: AngularFireDatabase, private route: ActivatedRoute) {}
 
- constructor(db: AngularFireDatabase) {
-  this.scores = db.list('/scores/2017/P2', {
-   query: {
-    orderByChild: 'total'
-   }
-  });
-  this.scores.subscribe(console.log);
- }
+  ngOnInit() {
+    this.route.queryParams.subscribe((params: Params) => {
+      this.selectedWeek = params.week || '1';
+      this.scores = this.db.list('/scores/2017/' + this.selectedWeek, {
+        query: {
+          orderByChild: 'total'
+        }
+      });
+    });
+  }
 }
