58b144d8c31ac41f6e7dbbbdab3c5a30b33cfc21
Harvey Jones <harveyj@gmail.com>
2017-08-20 12:00:04 -0400

    Add a basic console which allows per-week benching setting

--- web3/src/app/console.component.ts
+++ web3/src/app/console.component.ts
@@ -1,5 +1,6 @@
 
 import * as firebase from 'firebase/app';
+import { ActivatedRoute, Params } from '@angular/router';
 import { AngularFireAuth } from 'angularfire2/auth';
 import { AngularFireAuthModule } from 'angularfire2/auth';
 import { AngularFireDatabase, FirebaseListObservable, FirebaseObjectObservable } from 'angularfire2/database';
@@ -10,12 +11,27 @@ import { Observable } from 'rxjs/Observable';
   templateUrl: './console.component.html',
 })
 export class ConsoleComponent {
-  user: Observable<firebase.User>;
-  constructor(private afAuth: AngularFireAuth) {
-    this.user = afAuth.authState;
-    this.user.subscribe(value => {
-      // if logged in redirect to /
-    });
+  benchings: FirebaseListObservable<any>;
+  selectedWeek = 'P1';
+  year = '2017';
+
+  constructor(private db: AngularFireDatabase, private route: ActivatedRoute) {}
+
+  ngOnInit() {
+    this.route.queryParams.subscribe((params: Params) => {
+      this.selectedWeek = params.week || 'P1';
+      this.benchings = this.db.list(`/tmp/${this.year}/${this.selectedWeek}/benchings`, {
+        query: {
+          orderByChild: 'total'
+        }
+      });
+    })
+  }
+
+  onClick(benching: any) {
+    benching.$value = !benching.$value;
+    this.db.object('/tmp/2017/' + this.selectedWeek + '/benchings/' + benching.$key)
+      .set(benching.$value);
   }
 }
 
