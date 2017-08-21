81a12355c84b0ea0675cce9fec93a0681ec0b0a2
Aerion <aerion@gmail.com>
2017-08-20 22:22:04 -0700

    Remove the old-style 'bqbl.' prefix. s/var/let/g.

--- functions/scoring.js
+++ functions/scoring.js
@@ -1,7 +1,12 @@
-bqbl = {};
+const SCORE_KEYS = [
+  'ATT', 'INT', 'INT6', 'INT6OT', 'FUM6', 'FUM6OT', 'FUML', 'TD', 'PASSYD',
+  'SACKYD', 'SACK', 'SAF', 'BENCH', 'FREEAGENT', 'FUM', 'PASSYD', 'CMP', 'LONG',
+  'RUSHYD', 'INT6OT'
+];
+
 
 exports.computeScore = function(stats) {
-  let components = bqbl.computeScoreComponents(stats);
+  let components = computeScoreComponents(stats);
   let lineScore =
       `${stats['CMP']}/${stats['ATT']},
        ${stats['PASSYD']} yd,
@@ -30,48 +35,45 @@ exports.computeScore = function(stats) {
  * An immutable data object.
  * @constructor
  */
-ScoreComponent = function(pointValue, description) {
-  /**
-   * @type {number}
-   * @const
-   */
-  this.pointValue = pointValue;
+class ScoreComponent {
+  constructor(pointValue, description) {
+    /**
+     * @type {number}
+     * @const
+     */
+    this.pointValue = pointValue;
 
-  /**
-   * @type {string}
-   * @const
-   */
-  this.description = description;
+    /**
+     * @type {string}
+     * @const
+     */
+    this.description = description;
+  }
 };
 
 /**
  * @return {!Array<!ScoreComponent>} A list of ScoreComponents.
  */
-bqbl.computeScoreComponents = function(qbScore) {
-  keys = [
-    'ATT', 'INT', 'INT6', 'INT6OT', 'FUM6', 'FUM6OT', 'FUML', 'TD', 'PASSYD',
-    'SACKYD', 'SACK', 'SAF', 'BENCH', 'FREEAGENT', 'FUM', 'PASSYD', 'CMP', 'LONG',
-    'RUSHYD', 'INT6OT'
-  ];
+function computeScoreComponents(qbScore) {
   // Zero out any undefined keys
-  for (var i = 0; i < keys.length; i++) {
-    qbScore[keys[i]] = qbScore[keys[i]] || 0;
-  }
-  var pointsList = [
-    bqbl.simpleMultiple(25, qbScore['INT6'] - qbScore['INT6OT'],
+  SCORE_KEYS.forEach((k) => {
+    qbScore[k] = qbScore[k] || 0;
+  });
+  let pointsList = [
+    simpleMultiple(25, qbScore['INT6'] - qbScore['INT6OT'],
                         'INT returned for TD'),
-    bqbl.simpleMultiple(5, qbScore['INT'] - qbScore['INT6'], 'INT'),
-    bqbl.simpleMultiple(25, qbScore['FUM6'], 'fumble lost for TD'),
-    bqbl.simpleMultiple(5, qbScore['FUML'] - qbScore['FUM6'], 'fumble lost'),
-    bqbl.simpleMultiple(2, qbScore['FUM'] - qbScore['FUML'], 'fumble kept'),
-    bqbl.turnoverPoints(qbScore),
-    bqbl.touchdownPoints(qbScore['TD']),
-    bqbl.passingYardPoints(qbScore['PASSYD'] + qbScore['SACKYD']),
-    bqbl.completionRatePoints(qbScore['CMP'], qbScore['ATT']),
-    bqbl.simpleMultiple(1, qbScore['SACK'], 'sacked'),
-    bqbl.simpleMultiple(20, qbScore['SAF'], 'QB at fault for safety'),
-    bqbl.simpleMultiple(35, qbScore['BENCH'], 'QB benched'),
-    bqbl.simpleMultiple(20, qbScore['FREEAGENT'], 'free agent starter')
+    simpleMultiple(5, qbScore['INT'] - qbScore['INT6'], 'INT'),
+    simpleMultiple(25, qbScore['FUM6'], 'fumble lost for TD'),
+    simpleMultiple(5, qbScore['FUML'] - qbScore['FUM6'], 'fumble lost'),
+    simpleMultiple(2, qbScore['FUM'] - qbScore['FUML'], 'fumble kept'),
+    turnoverPoints(qbScore),
+    touchdownPoints(qbScore['TD']),
+    passingYardPoints(qbScore['PASSYD'] + qbScore['SACKYD']),
+    completionRatePoints(qbScore['CMP'], qbScore['ATT']),
+    simpleMultiple(1, qbScore['SACK'], 'sacked'),
+    simpleMultiple(20, qbScore['SAF'], 'QB at fault for safety'),
+    simpleMultiple(35, qbScore['BENCH'], 'QB benched'),
+    simpleMultiple(20, qbScore['FREEAGENT'], 'free agent starter')
   ];
   if (qbScore['LONG'] < 25)
     pointsList.push(new ScoreComponent(10, 'no pass of 25+ yards'));
@@ -82,21 +84,21 @@ bqbl.computeScoreComponents = function(qbScore) {
   return pointsList.filter(function(x) { return x.pointValue != 0; });
 };
 
-bqbl.turnoverPoints = function(qbScore) {
-  var totalTurnovers = qbScore['INT'] + qbScore['FUML'];
-  var points = 0;
+function turnoverPoints(qbScore) {
+  let totalTurnovers = qbScore['INT'] + qbScore['FUML'];
+  let points = 0;
   if (totalTurnovers == 3) points = 10;
   else if (totalTurnovers == 4) points = 20;
   else if (totalTurnovers >= 5) points = 32;
   // 1.5x points for subsequent turnovers.
-  for (var to = 6; to <= totalTurnovers; to++) {
+  for (let to = 6; to <= totalTurnovers; to++) {
     points = points + points / 2;
   }
   return new ScoreComponent(points, totalTurnovers + '-turnover game');
 };
 
-bqbl.passingYardPoints = function(yards) {
-  var points, rangeString;
+function passingYardPoints(yards) {
+  let points, rangeString;
   if (yards < 100) { points = 25; rangeString = 'under 100'; }
   else if (yards < 150) { points = 12; rangeString = 'under 150'; }
   else if (yards < 200) { points = 6; rangeString = 'under 200'; }
@@ -107,9 +109,9 @@ bqbl.passingYardPoints = function(yards) {
   return new ScoreComponent(points, rangeString + ' passing yards');
 };
 
-bqbl.completionRatePoints = function(completions, attempts) {
-  var completionRate = completions / attempts;
-  var points, rangeString;
+function completionRatePoints(completions, attempts) {
+  let completionRate = completions / attempts;
+  let points, rangeString;
   if (completionRate < 0.3) { points = 25; rangeString = 'under 30%'; }
   else if (completionRate < 0.4) { points = 15; rangeString = 'under 40%'; }
   else if (completionRate < 0.5) { points = 5; rangeString = 'under 50%'; }
@@ -117,13 +119,13 @@ bqbl.completionRatePoints = function(completions, attempts) {
   return new ScoreComponent(points, rangeString + ' completion rate');
 };
 
-bqbl.touchdownPoints = function(tds) {
-  var points;
+function touchdownPoints(tds) {
+  let points;
   if (tds == 0) points = 10;
   else if (tds < 3) points = 0;
   else points = -5;
   // Penalty doubled for each TD beyond 3.
-  for (var td = 4; td <= tds; td++) {
+  for (let td = 4; td <= tds; td++) {
     points *= 2;
   }
   return new ScoreComponent(points, tds + '-touchdown game');
@@ -137,7 +139,7 @@ bqbl.touchdownPoints = function(tds) {
  *     for the singular form of Y.
  * @return {!ScoreComponent} A ScoreComponent for the points described.
  */
-bqbl.simpleMultiple = function(pointsPer, quantity, description) {
+function simpleMultiple(pointsPer, quantity, description) {
   quantity = quantity || 0;
   if (quantity != 1) {
     description = quantity + 'x ' + description;
