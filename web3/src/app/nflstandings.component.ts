7640c65557c5f8e3f7f0fc05339d4f46f037d943
Aerion <aerion@gmail.com>
2017-09-07 00:36:36 -0700

    Ignore preseason scores. Render correctly when no scores are present.

--- web3/src/app/nflstandings.component.ts
+++ web3/src/app/nflstandings.component.ts
@@ -5,6 +5,8 @@ import { AngularFireDatabase } from 'angularfire2/database';
 import { Component } from '@angular/core';
 import { MdlDefaultTableModel } from '@angular-mdl/core';
 
+import { ConstantsService } from './constants.service';
+
 @Component({
   templateUrl: './nflstandings.component.html',
 })
@@ -16,12 +18,17 @@ export class NFLStandingsComponent {
   scoresByTeam = {};
   scores247ByTeam = {};
 
-  constructor(private db: AngularFireDatabase) {}
+  constructor(private db: AngularFireDatabase,
+              private constants: ConstantsService) {}
 
   ngOnInit() {
-    this.db.object(`/scores/${this.year}`).subscribe((d) => {
+    this.db.list(`/scores/${this.year}`).subscribe((d) => {
       let scores = {};
-      Object.entries(d).forEach(([weekNum, week]) => {
+      d.forEach((week) => {
+        const weekNum = week.$key;
+        if (weekNum.startsWith('P')) {
+          return;
+        }
         Object.entries(week).forEach(([name, score]) => {
           if (!(name in scores)) {
             scores[name] = {};
@@ -32,9 +39,9 @@ export class NFLStandingsComponent {
       this.scoresByTeam = scores;
       this.computeScores();
     });
-    this.db.object(`/scores247/${this.year}`).subscribe((d) => {
+    this.db.list(`/scores247/${this.year}`).subscribe((d) => {
       let scores = {};
-      Object.values(d).forEach((entry) => {
+      d.forEach((entry) => {
         const name = entry['team'];
         if (!(name in scores)) {
           scores[name] = 0;
@@ -50,10 +57,6 @@ export class NFLStandingsComponent {
       {key: 'total', name: 'Total', sortable: true, numeric: true},
       {key: '24x7', name: '24/7', sortable: true, numeric: true},
     ];
-    for (let i = 0; i <= 4; i++) {
-      columns.push(
-        {key: `P${i}`, name: `P${i}`, sortable: true, numeric: true});
-    }
     for (let i = 1; i <= 16; i++) {
       columns.push({key: `${i}`, name: `${i}`, sortable: true, numeric: true});
     }
@@ -62,18 +65,19 @@ export class NFLStandingsComponent {
 
   computeScores() {
     let scores = {};
-    Object.entries(this.scoresByTeam).forEach(([name, weekScores]) => {
-      let pts247 = this.scores247ByTeam[name] || 0;
+    this.constants.getAllTeams().forEach((team) => {
+      let teamScores = this.scoresByTeam[team] || {};
+      let pts247 = this.scores247ByTeam[team] || 0;
       let total = pts247;
-      scores[name] = {
-        'team': name,
+      scores[team] = {
+        'team': team,
         '24x7': pts247,
       };
-      Object.entries(weekScores).forEach(([weekNum, points]) => {
-        scores[name][weekNum] = points;
+      Object.entries(teamScores).forEach(([weekNum, points]) => {
+        scores[team][weekNum] = points;
         total += points;
       });
-      scores[name]['total'] = total;
+      scores[team]['total'] = total;
     });
     let sorted = Object.values(scores);
     sorted.sort((a, b) => a['team'].localeCompare(b['team']));
