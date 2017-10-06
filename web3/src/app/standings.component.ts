4c2592a021925438d666955af5b854263023bec1
Aerion <aerion@gmail.com>
2017-10-05 23:12:41 -0700

    Continued standings page refactoring and cleanup.
    This eliminates some intermediate data structures that were mostly just confusing.

--- web3/src/app/standings.component.ts
+++ web3/src/app/standings.component.ts
@@ -5,6 +5,7 @@ import { Component } from '@angular/core';
 import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/observable/combineLatest';
 import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/take';
 import { User } from './structs'
 import { TeamScore } from './team-score';
 import { ConstantsService } from './constants.service';
@@ -16,9 +17,7 @@ import * as paths from './paths';
   styleUrls: ['./standings.component.css']
 })
 export class StandingsComponent {
-  userDataList: FirebaseListObservable<any>;
   db: AngularFireDatabase;
-  leagueIdToUsers = new Map<string, User[]>();
   leagueIdToName = new Map<string, string>();
   leagues = new Map<string, Observable<UserEntry[]>>();
   year = '2017';
@@ -30,18 +29,25 @@ export class StandingsComponent {
               private constants: ConstantsService) {
     this.db = db;
 
-    this.userDataList = this.db.list(paths.getUsersPath());
-    this.userDataList.subscribe(users => {
-      this.leagueIdToUsers = new Map();
-      for (const user of users) {
-        const league = this.leagueIdToUsers.get(user.leagueId) || [];
-        league.push(user);
-        this.leagueIdToUsers.set(user.leagueId, league);
-
-        this.leagueIdToName.set(user.leagueId, user.leagueName);
-      }
-      this.updateScores();
-    });
+    // Take only one value. We expect the user list to be constant over the
+    // course of a season, so there's no need to subscribe to changes.
+    const userDataList = this.db.list(paths.getUsersPath()).take(1);
+    userDataList
+      .subscribe((users) => {
+        const leagueIdToUsers: Map<string, User[]> = new Map();
+        for (const user of users) {
+          const league = leagueIdToUsers.get(user.leagueId) || [];
+          league.push(user);
+          leagueIdToUsers.set(user.leagueId, league);
+          // TODO(aerion): This doesn't belong here.
+          this.leagueIdToName.set(user.leagueId, user.leagueName);
+        }
+        const leagues: Map<string, Observable<UserEntry[]>> = new Map();
+        leagueIdToUsers.forEach((leagueUsers, leagueId) => {
+          leagues.set(leagueId, this.scoresForLeague(leagueId, leagueUsers));
+        });
+        this.leagues = leagues;
+      });
   }
 
   ngOnInit() {
@@ -58,66 +64,57 @@ export class StandingsComponent {
     return this.leagues.get(key);
   }
 
-  // TODO doing a join by hand feels terrible.
-  updateScores(): void {
-    const leagueToUserEntries: Map<string, Observable<UserEntry>[]> = new Map();
-    for (const leagueKey of Array.from(this.leagueIdToUsers.keys())) {
-      for (const user of this.leagueIdToUsers.get(leagueKey)) {
-        const allScores: Observable<number>[] = [];
-        const weeks: Observable<any>[] = [];
-        for (const userWeek of user.weeks) {
-          const scoresForWeek = [];
-          for (const userTeam of userWeek.teams) {
-            if (userTeam.selected) {
-              const score =
-                this.scoreService.scoreFor(userWeek.id, userTeam.name);
-              scoresForWeek.push(score.map((s) => {
-                if (!s) {
-                  return null;
-                }
-                return {
-                  name: userTeam.name,
-                  score: s.total,
-                };
-              }));
-              allScores.push(score.map((s) => s ? s.total : 0));
-            }
-          }
-          if (scoresForWeek.length > 0) {
-            weeks.push(
-              Observable.combineLatest(scoresForWeek)
-                .map((arr) => {
-                  return {
-                    name: `Week ${userWeek.id}`,
-                    scores: arr.filter(v => !!v),
-                  }
-                }));
+  scoresForLeague(leagueKey: string, users: User[]): Observable<UserEntry[]> {
+    const userEntries: Observable<UserEntry>[] = [];
+    for (const user of users) {
+      const allScores: Observable<number>[] = [];
+      const weeks: Observable<any>[] = [];
+      for (const userWeek of user.weeks) {
+        const scoresForWeek = [];
+        for (const userTeam of userWeek.teams) {
+          if (userTeam.selected) {
+            const score =
+              this.scoreService.scoreFor(userWeek.id, userTeam.name);
+            scoresForWeek.push(score.map((s) => {
+              if (!s) {
+                return null;
+              }
+              return {
+                name: userTeam.name,
+                score: s.total,
+              };
+            }));
+            allScores.push(score.map((s) => s ? s.total : 0));
           }
         }
-        // TODO(aerion): Just make the whole 'leagues' structure async.
-        const scoresArr = Observable.combineLatest(allScores);
-        const weeksArr: Observable<WeekEntry[]> =
-          Observable.combineLatest(weeks);
-        const userRow: Observable<UserEntry> =
-          Observable.combineLatest([scoresArr, weeksArr])
-            .map(([scores, weeks]) => {
-              return {
-                'name': user.name,
-                'total': scores.reduce((a, b) => a + b, 0),
-                'weeks': weeks.filter((v) => v.scores.length > 0),
-              };
-            });
-        const league = leagueToUserEntries.get(leagueKey) || [];
-        league.push(userRow);
-        leagueToUserEntries.set(leagueKey, league);
+        if (scoresForWeek.length > 0) {
+          weeks.push(
+            Observable.combineLatest(scoresForWeek)
+              .map((arr) => {
+                return {
+                  name: `Week ${userWeek.id}`,
+                  scores: arr.filter(v => !!v),
+                }
+              }));
+        }
       }
+
+      const scoresArr = Observable.combineLatest(allScores);
+      const weeksArr: Observable<WeekEntry[]> =
+        Observable.combineLatest(weeks);
+      const userEntry: Observable<UserEntry> =
+        Observable.combineLatest([scoresArr, weeksArr])
+          .map(([scores, weeks]) => {
+            return {
+              'name': user.name,
+              'total': scores.reduce((a, b) => a + b, 0),
+              'weeks': weeks.filter((v) => v.scores.length > 0),
+            };
+          });
+      userEntries.push(userEntry);
     }
 
-    this.leagues = new Map();
-    for (const key of Array.from(this.leagueIdToUsers.keys())) {
-      this.leagues.set(key,
-                       Observable.combineLatest(leagueToUserEntries.get(key)));
-    }
+    return Observable.combineLatest(userEntries);
   }
 }
 
