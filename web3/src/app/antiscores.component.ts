a088b32d6f1735cdc07d1cd507d7b2b5d3983e6c
Harvey Jones <harveyj@gmail.com>
2017-12-28 20:52:07 -0500

    Clean up unused imports

--- web3/src/app/antiscores.component.ts
+++ web3/src/app/antiscores.component.ts
@@ -1,17 +1,10 @@
 import 'rxjs/add/observable/of';
 import 'rxjs/add/operator/map';
-import { ActivatedRoute, Router, NavigationEnd, Event, Params } from '@angular/router';
-import { AngularFireAuthModule } from 'angularfire2/auth';
-import { AngularFireDatabase, FirebaseListObservable, FirebaseObjectObservable } from 'angularfire2/database';
 import { Component } from '@angular/core';
 import { Observable } from 'rxjs/Observable';
 
-import * as paths from './paths';
-import { ConstantsService } from './constants.service';
-import { League, User } from './structs';
 import { ScoreService, LeagueScore } from './score.service';
-import { TeamScore } from './team-score';
-import { WeekService } from './week.service';
+
 
 @Component({
   templateUrl: './scores.component.html',
--- web3/src/app/scores.component.ts
+++ web3/src/app/scores.component.ts
@@ -1,15 +1,9 @@
 import 'rxjs/add/observable/of';
 import 'rxjs/add/operator/map';
-import { AngularFireDatabase, FirebaseListObservable, FirebaseObjectObservable } from 'angularfire2/database';
 import { Component } from '@angular/core';
 import { Observable } from 'rxjs/Observable';
 
-import * as paths from './paths';
-import { ConstantsService } from './constants.service';
-import { League, User } from './structs';
 import { ScoreService, LeagueScore } from './score.service';
-import { TeamScore } from './team-score';
-import { WeekService } from './week.service';
 
 @Component({
   templateUrl: './scores.component.html',
