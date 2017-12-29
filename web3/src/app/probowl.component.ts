2836ffd550637b14b6df960253616af2e026432a
Harvey Jones <harveyj@gmail.com>
2017-12-29 15:00:45 -0500

    Clean up lots of unused stuff

--- web3/src/app/probowl.component.ts
+++ web3/src/app/probowl.component.ts
@@ -2,18 +2,16 @@ import 'rxjs/add/observable/of';
 import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/take';
 import * as firebase from 'firebase/app';
-import { ActivatedRoute, Router, NavigationEnd, Event, Params } from '@angular/router';
 import { AngularFireAuth } from 'angularfire2/auth';
 import { AngularFireAuthModule } from 'angularfire2/auth';
+import { Router } from '@angular/router';
 import { AngularFireDatabase, FirebaseListObservable, FirebaseObjectObservable } from 'angularfire2/database';
 import { Component, Input } from '@angular/core';
-import { LeagueRules } from './schema'
-import { MdlSnackbarService } from '@angular-mdl/core';
 import { Observable } from 'rxjs/Observable';
 
 import * as paths from './paths';
 import { ConstantsService } from './constants.service';
-import { ScoreService } from './score.service';
+import { ScoreService, LeagueScore } from './score.service';
 import { TeamScore } from './team-score';
 import { User, Week, TeamEntry, League } from './structs';
 
@@ -22,20 +20,15 @@ import { User, Week, TeamEntry, League } from './structs';
   styleUrls: ['./probowl.component.css']
 })
 export class ProBowlComponent {
-  userData: FirebaseObjectObservable<any>;
-  userDataSnapshot: any;
   user: Observable<firebase.User>;
   uid: string;
   teams: any[]; 
 
   leagues: Observable<LeagueScore[]>;
-  year: Observable<string>;
-
 
   constructor(private db: AngularFireDatabase,
               private afAuth: AngularFireAuth,
               private router: Router,
-              private mdlSnackbarService: MdlSnackbarService,
               private constants: ConstantsService,
               private scoreService: ScoreService,
               ) {
@@ -44,15 +37,14 @@ export class ProBowlComponent {
       if (!value) {
         return;
       }
-      this.userData = this.db.object(paths.getUserPath(value.uid));
-      this.userData.subscribe(userData => {
-        this.userDataSnapshot = userData;
+      let userData = this.db.object(paths.getUserPath(value.uid));
+      userData.subscribe(userData => {
         if (!userData.$exists()) {
           this.router.navigate(['/newuser']);
         }
 
-        if(this.userDataSnapshot['probowl']) {
-          this.teams = this.userDataSnapshot['probowl'].teams;
+        if(userData['probowl']) {
+          this.teams = userData['probowl'].teams;
         } else {
           this.teams = [{name: "ARI"},{name: "CLE"},{name: "DET"},{name: "HOU"},{name: "PIT"},{name: "SEA"}];
         }
@@ -63,25 +55,13 @@ export class ProBowlComponent {
   }
 
   ngOnInit() {
-    this.year = Observable.of('2017');
     this.leagues = this.scoreService.getLeaguesProBowl();
   }
 
   onChange() {
-    console.log("change!")
     this.db
       .object(paths.getUserPath(this.uid) + '/probowl/teams')
       .set(this.teams);
   }
 
-}
-
-class LeagueScore {
-  name: string;
-  players: Observable<PlayerScore[]>;
-}
-
-class PlayerScore {
-  name: string;
-  scores: TeamScore[];
 }
