7e3d108376a49e364e9d29f31b8fe0592f2acac7
Aerion <aerion@gmail.com>
2018-09-09 15:39:00 -0700

    Use the new score breakdown data in the UI.
    The component descriptions never really belonged in the scoring code. This moves more UI decisions to the UI so that we can display them in a more flexible way.

--- web3/src/app/nfl-scores/nfl-score-card.component.ts
+++ web3/src/app/nfl-scores/nfl-score-card.component.ts
@@ -1,5 +1,7 @@
 import { Component, Input } from '@angular/core';
 
+import { ScoreBreakdown, Range } from './breakdown';
+
 @Component({
   selector: 'nfl-score-card',
   templateUrl: './nfl-score-card.component.html',
@@ -19,9 +21,17 @@ export class NflScoreCardComponent {
   }
 
   getComponents() {
+    return breakdownToComponents(this.getBreakdown());
+  }
+
+  getBreakdown(): ScoreBreakdown {
     return this.projectScores && this.score['projection'] ?
-        this.score['projection']['components'] :
-        this.score['components'];
+        this.score['projection']['breakdown'] :
+        this.score['breakdown'];
+  }
+
+  getBreakdownEntry(entryName: string) {
+    return this.getBreakdown()[entryName];
   }
 
   // TODO(aerion): Factor this out (probably as an instance method of a
@@ -54,4 +64,117 @@ export class NflScoreCardComponent {
     }
     return `${aName} ${aScore} @ ${hName} ${hScore}`;
   }
+}
+
+/**
+ * An entry in the box score. This is something of a bridge between the old UI
+ * and future UIs. The old UI expressed the score as a series of ScoreComponents
+ * whose values were added to produce the total score. We can develop a more
+ * flexible UI, but during the transition period it's easy-ish to transform the
+ * score Breakdown into a series of ScoreComponents.
+ */
+declare interface ScoreComponent {
+  /** Human-readable description. */
+  desc: string;
+  /** Point value. */
+  value: number;
+}
+
+function breakdownToComponents(breakdown: ScoreBreakdown) {
+  const components = [];
+  // TODO(aerion): Individual turnover values aren't stored in the breakdown so
+  // we have to recompute them here. Re-think this schema.
+  components.push(simpleMultiple(
+      25, breakdown.turnover.types.int6, 'INT returned for TD'));
+  components.push(simpleMultiple(
+      5, breakdown.turnover.types.int - breakdown.turnover.types.int6, 'INT'));
+  components.push(simpleMultiple(
+      25, breakdown.turnover.types.fum6, 'fumble lost for TD'));
+  components.push(simpleMultiple(
+      5, breakdown.turnover.types.fuml - breakdown.turnover.types.fum6,
+      'fumble lost'));
+  // An OT turnover-6 is worth 50 points total: 25 for the turnover itself, plus
+  // 25 bonus points for being in OT. (But again, ideally this code wouldn't
+  // have to make that distinction.)
+  components.push(simpleMultiple(
+      25, breakdown.turnover.types.ot6, 'turnover-TD in OT'));
+  components.push(simpleMultiple(
+      2, breakdown.fumbleKept.count, 'fumble kept')),
+  components.push({
+    desc: `${breakdown.turnover.count}-turnover bonus`,
+    value: breakdown.turnover.bonusValue,
+  });
+
+  components.push({
+    desc: `${breakdown.touchdown.count}-touchdown game`,
+    value: breakdown.touchdown.value,
+  });
+
+  components.push({
+    desc: rangeString(breakdown.passingYardage.range) + ' passing yards',
+    value: breakdown.passingYardage.value,
+  });
+  components.push({
+    desc: rangeString(breakdown.completion.range) + '% completion rate',
+    value: breakdown.completion.value,
+  });
+  components.push({
+    desc: 'longest pass ' + rangeString(breakdown.longPass.range) + ' yards',
+    value: breakdown.longPass.value,
+  });
+  components.push({
+    desc: rangeString(breakdown.rushingYardage.range) + ' rushing yards',
+    value: breakdown.rushingYardage.value,
+  });
+
+  components.push({
+    desc: breakdown.sack.count == 1 ? '1 sack' : `${breakdown.sack.count} sacks`,
+    value: breakdown.sack.value,
+  });
+
+  components.push({
+    desc: `${breakdown.safety.count}x QB at fault for safety`,
+    value: breakdown.safety.value,
+  });
+
+  components.push({
+    desc: `${breakdown.bench.count}x QB benched`,
+    value: breakdown.bench.value,
+  });
+
+  for (let passer of breakdown.passerRating.passers) {
+    components.push({
+      desc: `${passer.rating.toFixed(1)} passer rating (${passer.name})`,
+      value: passer.value,
+    });
+  }
+
+  return components.filter(c => c.value != 0);
+}
+
+/**
+ * Creates a ScoreComponent representing a simple "X points per Y" value.
+ * @param {number} pointsPer How many points each Y is worth ("X").
+ * @param {number} quantity How many Ys there are.
+ * @param {string} description A description of what Y is. Probably should be
+ *     for the singular form of Y.
+ * @return {!ScoreComponent} A ScoreComponent for the points described.
+ */
+function simpleMultiple(pointsPer, quantity, description): ScoreComponent {
+  quantity = quantity || 0;
+  if (quantity != 1) {
+    description = quantity + 'x ' + description;
+  }
+  return {desc: description, value: quantity * pointsPer};
+}
+
+function rangeString(range: Range) {
+  const {min, max} = range;
+  if (min === undefined) {
+    return `≤ ${max}`;
+  }
+  if (max === undefined) {
+    return `≥ ${min}`;
+  }
+  return `${min}-${max}`;
 }
