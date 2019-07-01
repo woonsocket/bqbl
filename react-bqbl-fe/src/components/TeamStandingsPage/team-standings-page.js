3264189740b0105637b5b7eb30d952ebfee210ea
Harvey Jones <harveyj@gmail.com>
2019-06-30 18:34:51 -0700

    Table-ify team standings

--- react-bqbl-fe/src/components/TeamStandingsPage/team-standings-page.js
+++ react-bqbl-fe/src/components/TeamStandingsPage/team-standings-page.js
@@ -1,6 +1,12 @@
 import React, { Component } from 'react';
 import { withRouter } from 'react-router-dom';
 import { compose } from 'recompose';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
 
 import { withFirebase } from '../Firebase';
 
@@ -18,9 +24,9 @@ class TeamStandingsPageBase extends Component {
   componentDidMount() {
     this.props.firebase.scores_year('2018').on('value', snapshot => {
       let weekMap = {};
-      for(let [weekId, weekVal] of Object.entries(snapshot.val())) {
-        for(let [teamId, teamVal] of Object.entries(weekVal)) {
-          weekMap[teamId] = weekMap[teamId] || {}; 
+      for (let [weekId, weekVal] of Object.entries(snapshot.val())) {
+        for (let [teamId, teamVal] of Object.entries(weekVal)) {
+          weekMap[teamId] = weekMap[teamId] || {};
           weekMap[teamId][weekId] = teamVal.total
         }
       }
@@ -30,14 +36,31 @@ class TeamStandingsPageBase extends Component {
 
   render() {
     return (
-      <React.Fragment>
-        {Object.entries(this.state.allScores).map((v, k) => 
-          <div><span>{v[0]}</span>
-          {WEEK_IDS.map(id => 
-          (<span>{JSON.stringify(v[1][id])}</span>))}
-          </div>
-          )}
-      </React.Fragment>
+      <Paper>
+        <Table>
+          <TableHead>
+            <TableRow>
+              <TableCell>Team</TableCell>
+              {WEEK_IDS.map((v, k) =>
+                <TableCell align="right">{v}</TableCell>
+              )}
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {Object.entries(this.state.allScores).map((teamVal, teamKey) => (
+              <TableRow>
+                <TableCell>{teamVal[0]}</TableCell>
+                {WEEK_IDS.map((weekId) =>
+                  <TableCell>
+                    {JSON.stringify(teamVal[1][weekId])}
+                  </TableCell>
+                )}
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </Paper>
+
     );
   }
 }
