5befcc074c9ec9e2bb62e5c36cf3e5df31c4da0c
Harvey Jones <harveyj@gmail.com>
2019-07-09 13:28:44 -0700

    Check in a test!!1! Update some deps

---
+++ react-bqbl-fe/src/components/WeekTeam/week-team-row.test.js
@@ -0,0 +1,25 @@
+/* eslint-disable no-undef */
+import React from 'react';
+import '@testing-library/jest-dom';
+import { render, fireEvent, cleanup } from '@testing-library/react'
+
+import WeekTeamRow from './week-team';
+
+let container;
+
+beforeEach(() => {
+  container = document.createElement('div');
+  document.body.appendChild(container);
+});
+
+afterEach(() => {
+  document.body.removeChild(container);
+  container = null;
+});
+
+it('renders', () => {
+  const week = {starts: [{name: "NYJ", total: 6}]};
+  const weekId = "1";
+  const { getByText } = render(<WeekTeamRow week={week} weekId={weekId} />);
+  expect(getByText(/^Week/).textContent).toContain('Week 1')
+});
