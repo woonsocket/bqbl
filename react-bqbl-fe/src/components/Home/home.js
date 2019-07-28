3eaf0175e73aadd62bd94b127d61d5ea848968e7
Harvey Jones <harveyj@gmail.com>
2019-07-28 09:41:00 -0700

    Add a reasonable splash screen we can direct people to.

--- react-bqbl-fe/src/components/Home/home.js
+++ react-bqbl-fe/src/components/Home/home.js
@@ -1,8 +1,14 @@
 import React from 'react';
-
+import SignIn from '../SignIn/sign-in';
+import "./home.css"
 const Home = () => (
-  <div>
-    <h1>Home</h1>
+  <div className="splash">
+    <div>
+      The Fantasy Football League for Mean People
+    </div>
+    <div>
+      <SignIn />
+    </div>
   </div>
 );
 
