ade624e26836caf183977fb97f31cd43cf3d1997
Harvey Jones <harveyj@gmail.com>
2019-08-17 08:36:16 -0400

    Merge navigation and app

--- react-bqbl-fe/src/components/App/index.js
+++ react-bqbl-fe/src/components/App/index.js
@@ -9,7 +9,6 @@ import * as ROUTES from '../../constants/routes';
 import DraftPage from '../Draft/draft';
 import HomePage from '../Home/home';
 import LineupPage from '../Lineup/lineup';
-import Navigation from '../Navigation/navigation';
 import PlayerScorePage from '../PlayerScorePage/player-score-page';
 import PlayerStandingsPage from '../PlayerStandingsPage/player-standings-page';
 import ScorePage from '../TeamScorePage/team-score-page';
@@ -24,13 +23,17 @@ import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
+import { Link } from 'react-router-dom';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
+
 
 function App() {
 
   // eslint-disable-next-line
   const [auth, setAuth] = React.useState(true);
   const [drawerOpen, setDrawerOpen] = React.useState(false);
-
 
   function handleDrawerOpen() {
     setDrawerOpen(true);
@@ -86,4 +89,32 @@ function App() {
     </Router>
   );
 }
+
+// TODO: Line up with parameterized routes?
+const LINKS = [
+  { to: ROUTES.HOME, text: 'Home' },
+  { to: ROUTES.LINEUP, text: 'Lineup' },
+  { to: ROUTES.PLAYER_SCORES, text: 'Player Scores' },
+  { to: ROUTES.PLAYER_STANDINGS, text: 'Player Standings' },
+  { to: ROUTES.TEAM_SCORES, text: 'Team Scores' },
+  { to: ROUTES.TEAM_STANDINGS, text: 'Team Standings' },
+  { to: ROUTES.DRAFT, text: 'Draft' },
+];
+
+function Navigation(params) {
+  return (
+    <List>
+      {
+        LINKS.map((item, index) => (
+          <Link to={item.to} onClick={params.close} key={"link" + index}>
+            <ListItem button key={item.text}>
+              <ListItemText primary={item.text} />
+            </ListItem>
+          </Link>
+        ))
+      }
+    </List>
+  )
+}
+
 export default App;
