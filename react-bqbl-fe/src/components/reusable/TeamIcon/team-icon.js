a96f4d7f9eb37803f43030373f35aa4d1a31dacf
Harvey Jones <harveyj@gmail.com>
2019-09-07 07:26:34 -0400

    security fix

--- react-bqbl-fe/src/components/reusable/TeamIcon/team-icon.js
+++ react-bqbl-fe/src/components/reusable/TeamIcon/team-icon.js
@@ -10,7 +10,7 @@ function TeamIcon(props) {
   return (
     props.team && props.team !== 'none' &&
      <img src={
-        'http://i.nflcdn.com/static/site/7.5/img/logos/svg/' +
+        'https://i.nflcdn.com/static/site/7.5/img/logos/svg/' +
         'teams-matte/' + props.team + '.svg'}
       width={props.width}
       alt="{props.team}" />
