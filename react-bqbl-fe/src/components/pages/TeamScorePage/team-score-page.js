136e9527050046fa4e118e0a8ffb03b86e723446
Harvey Jones <harveyj@gmail.com>
2019-09-08 17:55:47 -0400

    team score updates
    fix styling on big screens
    sort scores by default (bool-settable for later)

--- react-bqbl-fe/src/components/pages/TeamScorePage/team-score-page.js
+++ react-bqbl-fe/src/components/pages/TeamScorePage/team-score-page.js
@@ -9,18 +9,24 @@ import TeamScoreCard from '../../reusable/TeamScoreCard/team-score-card';
 
 function TeamScorePageBase(props) {
   let [valsList, setValsList] = useState([])
+  const sortScores = true;
 
   useEffect(() => {
     props.firebase.scoresWeekPromise(props.year, props.week).then(
-      scoresWeek => setValsList(scoresWeek));
+      scoresWeek => {
+        if (sortScores) {
+          scoresWeek = scoresWeek.sort((team, team2) => team2.total - team.total);
+        }
+        setValsList(scoresWeek)
+      });
   }, [props.firebase, props.league, props.year, props.week]);
 
   return (
-    <React.Fragment>
+    <div style={{textAlign: 'center'}}>
       {valsList.map(score => (
         <TeamScoreCard score={score} key={score.teamName} />
       ))}
-    </React.Fragment>
+    </div>
   );
 }
 
