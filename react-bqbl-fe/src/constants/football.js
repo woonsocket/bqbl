a312a8f4ca24f58816cc6b2c8d83d7292daef699
Aerion <aerion@gmail.com>
2019-09-29 19:51:52 -0700

    LOS AN-GE-LES      SUPER CHAAAARGERS

--- react-bqbl-fe/src/constants/football.js
+++ react-bqbl-fe/src/constants/football.js
@@ -1,6 +1,6 @@
 export const ALL_TEAMS = ["ARI", "ATL", "BAL", "BUF", "CAR", "CHI", "CIN", "CLE", "DAL", "DEN", "DET",
-  "GB", "HOU", "IND", "JAX", "KC", "LA", "MIA", "MIN", "NE", "NO", "NYG", "NYJ",
-  "OAK", "PHI", "PIT", "SD", "SEA", "SF", "TB", "TEN", "WAS"];
+  "GB", "HOU", "IND", "JAX", "KC", "LA", "LAC", "MIA", "MIN", "NE", "NO", "NYG", "NYJ",
+  "OAK", "PHI", "PIT", "SEA", "SF", "TB", "TEN", "WAS"];
 
 export const WEEK_IDS = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16",];
 
