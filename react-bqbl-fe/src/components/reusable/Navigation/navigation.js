d4ed85fb85a7ff557536e4dc9b0d19833c8e5bac
Aerion <aerion@gmail.com>
2019-12-29 01:48:51 -0800

    Rename Pro Bowl lineup nav menu option.

--- react-bqbl-fe/src/components/reusable/Navigation/navigation.js
+++ react-bqbl-fe/src/components/reusable/Navigation/navigation.js
@@ -20,7 +20,7 @@ const TWENTYFOUR = '/247';
 export const LINKS = {
   HOME: { path: HOME, text: 'Home' },
   LINEUP: { path: LINEUP, text: 'Lineup' },
-  PROBOWL: { path: PROBOWL, text: 'Pro Bowl' },
+  PROBOWL: { path: PROBOWL, text: 'Pro Bowl Lineup' },
   PROBOWL_SCORES: { path: PROBOWL_SCORES, text: 'Pro Bowl Scores' },
   PLAYER_SCORES: { path: PLAYER_SCORES, text: 'Player Scores' },
   PLAYER_STANDINGS: { path: PLAYER_STANDINGS, text: 'Player Standings' },
