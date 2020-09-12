d8608ab79791bbea881c65de2c6655f45c573246
Harvey Jones <harveyj@gmail.com>
2020-09-12 11:18:53 -0400

    this'll be a fun todo to resolve

--- react-bqbl-fe/src/constants/football.js
+++ react-bqbl-fe/src/constants/football.js
@@ -1,3 +1,4 @@
+// TODO: good lord...
 export const ALL_TEAMS = ["ARI", "ATL", "BAL", "BUF", "CAR", "CHI", "CIN", "CLE", "DAL", "DEN", "DET",
   "GB", "HOU", "IND", "JAX", "KC", "LA", "LAC", "MIA", "MIN", "NE", "NO", "NYG", "NYJ",
   "OAK", "PHI", "PIT", "SEA", "SF", "TB", "TEN", "WAS"];
