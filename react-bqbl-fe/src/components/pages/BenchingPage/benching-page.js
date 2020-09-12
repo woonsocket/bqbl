1ffbd1e58ad338a791d420938c7d95fecbc942da
Harvey Jones <harveyj@gmail.com>
2020-09-12 11:35:35 -0400

    clean up benching page

--- react-bqbl-fe/src/components/pages/BenchingPage/benching-page.js
+++ react-bqbl-fe/src/components/pages/BenchingPage/benching-page.js
@@ -1,23 +1,22 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useContext } from 'react';
 
-import { withFirebase } from '../../Firebase';
+import { FirebaseContext } from '../../Firebase';
 
-import { compose } from 'recompose';
-import { withRouter } from 'react-router-dom';
 import Table from '@material-ui/core/Table';
 import TableBody from '@material-ui/core/TableBody';
 import TableCell from '@material-ui/core/TableCell';
 import TableRow from '@material-ui/core/TableRow';
 import Checkbox from '@material-ui/core/Checkbox';
 
-function BenchingPageBase(props) {
+function BenchingPage(props) {
   let [events, setEvents] = useState({ passers: {} });
+  let firebase = useContext(FirebaseContext)
 
   useEffect(() => {
-    return props.firebase.getEventsThen(props.year, props.week, newEvents => {
+    return firebase.getEventsThen(props.year, props.week, newEvents => {
       setEvents(newEvents);
     });
-  }, [props.firebase, props.year, props.week]);
+  }, [firebase, props.year, props.week]);
 
   const handleChange = passerId => event => {
     const passer = events.passers[passerId];
@@ -25,7 +24,7 @@ function BenchingPageBase(props) {
     newOverrides[passer.team] = newOverrides[passer.team] || {};
     newOverrides[passer.team].benchings = newOverrides[passer.team].benchings || {};
     newOverrides[passer.team].benchings[passerId] = event.target.checked;
-    props.firebase.updateEventsOverrides(props.year, props.week, newOverrides);
+    firebase.updateEventsOverrides(props.year, props.week, newOverrides);
   };
 
   const getBenched = passerId => {
@@ -59,10 +58,5 @@ function BenchingPageBase(props) {
     </Table>
   );
 }
-
-const BenchingPage = compose(
-  withRouter,
-  withFirebase,
-)(BenchingPageBase);
 
 export default BenchingPage;
