e22d29a0e18f29d5e626c54294380afff4b427bb
Harvey Jones <harveyj@gmail.com>
2023-09-06 14:16:38 -0400

    add useragent and preseasons scrape capability to id scraper

--- nfl_scraper/mickey_ids.py
+++ nfl_scraper/mickey_ids.py
@@ -2,14 +2,17 @@ import requests
 import json
 import re
 
-def mickey_ids(yearId, weekId, dst):
+def mickey_ids(yearId, weekId, dst, preseason=False):
   url = 'https://www.espn.com/nfl/scoreboard/_/year/%s/seasontype/2/week/%s' %(yearId, weekId)
-  headers = {'Content-Type': 'text/html',}
+  if preseason:
+    url = 'https://www.espn.com/nfl/scoreboard/_/year/%s/seasontype/1/week/%s' %(yearId, weekId)
+  headers = {'Content-Type': 'text/html', 'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36'}
   response = requests.get(url, headers=headers)
   html = response.text
   pat = re.compile('href="/nfl/game/_/gameId/([0-9]{9})')
   ids = pat.findall(html)
   print(yearId, weekId, ids)
+  if preseason: weekId += 'P'
   year = dst.get(yearId, {})
   year[weekId] = ids
   dst[yearId] = year
@@ -18,9 +21,11 @@ def mickey_ids(yearId, weekId, dst):
 if __name__ == "__main__":
   dst = {}
   # ANNUAL
-  years = [str(y) for y in range(2009, 2023)]
+  years = [str(y) for y in range(2023, 2024)]
   try:
     for year in years:
+      for week in range(1, 6):
+        mickey_ids(year, str(week), dst, preseason=True)
       for week in range(1, 19):
         mickey_ids(year, str(week), dst)
   except: pass
