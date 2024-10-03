894c558dd6b9fdaa16a842f6a824046f0ea9c39c
Aerion <aerion@gmail.com>
2024-10-03 01:01:28 -0700

    Add passer stats to the benching page.

--- react-bqbl-fe/src/components/pages/BenchingPage/benching-page.jsx
+++ react-bqbl-fe/src/components/pages/BenchingPage/benching-page.jsx
@@ -1,26 +1,44 @@
 import Checkbox from '@mui/material/Checkbox';
+import PropTypes from 'prop-types';
 import Table from '@mui/material/Table';
 import TableBody from '@mui/material/TableBody';
 import TableCell from '@mui/material/TableCell';
 import TableRow from '@mui/material/TableRow';
 import React, { useContext, useEffect, useState } from 'react';
+import { useSelector } from "react-redux";
 import { useWeek, useYear } from '../../AppState';
 import { FirebaseContext } from '../../Firebase';
 
+import badBadge from './bad.png';
+
 function BenchingPage(props) {
-  let [events, setEvents] = useState({ passers: {} });
+  let [events, setEvents] = useState({});
+  let [passers, setPassers] = useState([]);
   let firebase = useContext(FirebaseContext)
+
+  const scores = useSelector((state) => state.scores);
+
   let year = useYear();
   let week = useWeek();
 
   useEffect(() => {
+    if (scores && scores[week]) {
+      let allPassers = {};
+      let scoresWeek = Object.entries(scores[week])
+        .forEach(([teamName, score]) => {
+          if (score.passers && Object.keys(score.passers).length >= 2) {
+            allPassers[teamName] = score.passers;
+          }
+        });
+      setPassers(allPassers);
+    }
     return firebase.getEventsThen(year, week, newEvents => {
       setEvents(newEvents);
     });
-  }, [firebase, year, week]);
+  }, [firebase, year, week, scores]);
 
-  const handleChange = passerId => event => {
-    const passer = events.passers[passerId];
+  const handleChange = (passerId, teamName) => event => {
+    const passer = passers[teamName][passerId];
     let newOverrides = JSON.parse(JSON.stringify(events.overrides || {}));
     newOverrides[passer.team] = newOverrides[passer.team] || {};
     newOverrides[passer.team].benchings = newOverrides[passer.team].benchings || {};
@@ -28,35 +46,67 @@ function BenchingPage(props) {
     firebase.updateEventsOverrides(year, week, newOverrides);
   };
 
-  const getBenched = passerId => {
-    const passer = events.passers[passerId];
-    if (!passer || !events.overrides || !events.overrides[passer.team] || !events.overrides[passer.team].benchings ) {
+  const isBenched = (passerId, teamId) => {
+    if (!events.overrides || !events.overrides[teamId] || !events.overrides[teamId].benchings ) {
       return false;
     }
-    let overrides = events.overrides[passer.team];
+    let overrides = events.overrides[teamId];
     return overrides.benchings[passerId];
   }
 
   return (
     <Table>
       <TableBody>
-        {Object.keys(events.passers).map((key) =>
-          <TableRow key={key}>
-            <TableCell align="left" >{events.passers[key].name}</TableCell>
-            <TableCell align="right">
-              <Checkbox
-                checked={getBenched(key)}
-                onChange={handleChange(key)}
-                value="test"
-                color="primary"
-                inputProps={{
-                  'aria-label': 'secondary checkbox',
-                }} />
-            </TableCell>
-          </TableRow>
+        {Object.entries(passers).flatMap(
+          ([teamId, teamPassers]) => (
+            <React.Fragment key={teamId}>
+              {Object.entries(teamPassers).map(
+                ([passerId, passer]) => (
+                  <TableRow key={passerId}>
+                    <TableCell align="left">
+                      <PasserDescription passer={passer} teamId={teamId} />
+                    </TableCell>
+                    <TableCell align="right">
+                      <Checkbox
+                        checked={isBenched(passerId, teamId)}
+                        onChange={handleChange(passerId, teamId)}
+                        value="test"
+                        color="primary"
+                        inputProps={{
+                          'aria-label': 'secondary checkbox',
+                        }} />
+                    </TableCell>
+                  </TableRow>
+                )
+              )}
+            </React.Fragment>
+          )
         )}
       </TableBody>
     </Table>
+  );
+}
+
+PasserDescription.propTypes = {
+  passer: PropTypes.object.isRequired,
+  teamId: PropTypes.string.isRequired,
+};
+
+function PasserDescription(props) {
+  const turnovers = props.passer.stats.int + props.passer.stats.fuml;
+  return (
+    // Halp, how do you CSS in React
+    <div style={{display: 'flex', flexDirection: 'row', alignItems: 'center'}}>
+      <div>
+        {props.passer.name} ({props.teamId})<br />
+        {props.passer.stats.cmp}/{props.passer.stats.att},&nbsp;
+        {props.passer.stats.netyds} yd,&nbsp;
+        {turnovers} turnover{turnovers != 1 ? 's' : ''}
+      </div>
+      {props.passer.isBad && <div style={{padding: '6px'}}>
+        <img src={badBadge} height="24" alt="BAD" />
+      </div>}
+    </div>
   );
 }
 
